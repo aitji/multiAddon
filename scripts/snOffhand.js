c5c5223016cd4dda0244d8641cf9fb8720c9cda1
Inw AitJi <jariya232560@gmail.com>
2024-06-20 19:29:16 +0700

    sn stack

--- scripts/snOffhand.js
+++ scripts/snOffhand.js
@@ -1,24 +1,26 @@
-import { EquipmentSlot, world } from "@minecraft/server";
-import { light } from "./_function";
+import { Container, EntityComponentTypes, EntityEquippableComponent, EntityInventoryComponent, EquipmentSlot, ItemStack, system, world } from "@minecraft/server";
+import { light, reName } from "./_function";
 import { get } from "./main";
 
 world.beforeEvents.itemUse.subscribe(data => {
-    if (world.getDynamicProperty('setting').split('')[8] === '0') return
+    const setting = world.getDynamicProperty('setting')
+    if (!setting || setting.charAt(8) === '0') return
+
     const { source, itemStack: item } = data
-    let itemStack = item
-    if (item) {
-        if (!source.isSneaking) return
-        const off = source.getComponent("equippable")?.getEquipment(EquipmentSlot.Offhand)
-        if (off) source.setDynamicProperty(`actionbar§:§r§7you already have item in offhand`, 3)
-        else {
-            const ty = itemStack?.typeId?.split('minecraft:')[1].toLowerCase()
-            if (light[ty]) {
-                source.setDynamicProperty(`actionbar§:§r§7you just wear: ${ty}`, 3)
-                source.runCommandAsync(`clear @s ${item.typeId} 0 1`)
-                if (get('inv')) source.runCommandAsync(`replaceitem entity @a slot.weapon.offhand 0 ${item.typeId} 1`)
-                else source.runCommandAsync(`replaceitem entity @s slot.weapon.offhand 0 ${item.typeId} 1`)
-                data.cancel = true
-            } else source.setDynamicProperty(`actionbar§:§r§7only support dynamic light item`, 3)
-        }
+    if (!item || !source.isSneaking) return
+
+    /** @type {ItemStack} */
+    const oHand = source.getComponent("equippable")?.getEquipment(EquipmentSlot.Offhand)
+    if (oHand && oHand?.typeId !== item?.typeId) source.setDynamicProperty('actionbar§:§r§7You already have an item in offhand', 3)
+    else {
+        const typeId = item.typeId?.split('minecraft:')[1]?.toLowerCase()
+        if (typeId && light[typeId]) {
+            const count = (oHand?.amount || 0) + 1
+            source.setDynamicProperty(`actionbar§:§r§7You just wore: ${reName(item?.typeId)} x${count}`, 3)
+            source.runCommandAsync(`clear @s ${item.typeId} 0 1`)
+            const target = get('inv') ? '@a' : '@s'
+            source.runCommandAsync(`replaceitem entity ${target} slot.weapon.offhand 0 ${item.typeId} ${count}`)
+            data.cancel = true
+        } else source.setDynamicProperty('actionbar§:§r§7Only support dynamic light items', 3)
     }
 })
